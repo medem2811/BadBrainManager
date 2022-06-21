17559e4a9676d4d37f98bc43a23ccc8290bc93aa
m.demeter <milo.demeter@softgarden.de>
2022-06-21 16:28:12 +0200

    Add a task, Simplified task

--- src/task/Task.java
+++ src/task/Task.java
@@ -8,25 +8,27 @@ public class Task {
 
     private String taskName;
 
-    private long startDate;
+    private String startDate;
 
-    private Date deadline;
+    private String deadline;
 
     public Task (String taskName, String deadline) {
         this.taskName = taskName;
-        parseDate(deadline);
-        this.startDate = new Date().getTime();
+        this.startDate = "today";
+        this.deadline = deadline;
     }
 
-    private void parseDate(String date) {
-        String format = "dd.MM.yyyy HH:mm";
-        SimpleDateFormat sdf = new SimpleDateFormat(format);
-
-        try {
-            this.deadline = sdf.parse(date);
-        } catch (ParseException e) {
-            System.err.println("ERROR: Could not parse the date");
-            e.printStackTrace();
-        }
+    public String getTaskName() {
+        return this.taskName;
     }
+
+    public String getDeadline() {
+        return this.deadline;
+    }
+
+    public String getStartDate() {
+        return this.startDate;
+    }
+
+
 }
